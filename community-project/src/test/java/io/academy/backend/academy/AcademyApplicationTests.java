e6f1d7ec752108d02389da959a91cde6f8f7bfed
Amadeu Lee <76532707+amadeulee@users.noreply.github.com>
2023-02-10 19:13:08 -0300

    Adding active profile

--- community-project/src/test/java/io/academy/backend/academy/AcademyApplicationTests.java
+++ community-project/src/test/java/io/academy/backend/academy/AcademyApplicationTests.java
@@ -2,12 +2,16 @@ package io.academy.backend.academy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "NoDB")
 class AcademyApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+
 	}
 
 }
